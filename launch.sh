87da6780858c71b79b545eb7608be5a5ae247d29
mikegreen1995 <mikegreen1995@outlook.com>
2020-04-05 12:15:29 +0100

    Launch script can now be launched from anywhere safely

--- launch.sh
+++ launch.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 echo "Launching Pelican Server..."
+ORIGINAL_DIRECTORY="`pwd`"
+LOCAL_DIRECTORY="`dirname ${0}`"
+cd ${LOCAL_DIRECTORY}
 VENV_DIR=venv
 if [[ ! -d "$VENV_DIR" ]]; then
     echo "$VENV_DIR directory not detected. Creating virtual environment..."
@@ -9,4 +12,5 @@ source ${VENV_DIR}/bin/activate
 pip3 install -r requirements.txt
 python3 src/pelicanserver.py
 deactivate
+cd ${ORIGINAL_DIRECTORY}
 echo "Pelican Server terminated."
