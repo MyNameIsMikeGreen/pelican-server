5d8e66a16f0213068394f893868d5a465026a91c
mikegreen1995 <mikegreen1995@outlook.com>
2020-04-25 22:50:29 +0100

    Activation/deactivation action response testing.

--- src/tests/test_pelicanserver.py
+++ src/tests/test_pelicanserver.py
@@ -3,22 +3,18 @@ import unittest
 from unittest.mock import Mock
 
 import pelicanserver
-import statusMonitor
+from statusMonitor import StatusMonitor, Status
 
 
 class PelicanServerTests(unittest.TestCase):
 
-    def setUp(self):
-        status_monitor = statusMonitor.StatusMonitor()
-        command_executor = Mock()
+    def setup_server(self, status_monitor=StatusMonitor(), command_executor=StatusMonitor):
         self.pelican_server = pelicanserver.PelicanServer(status_monitor, command_executor)
         self.pelican_server.app.config['TESTING'] = True
         self.app = self.pelican_server.app.test_client()
 
-    def tearDown(self):
-        pass
-
     def test_server_returns_static_homepage(self):
+        self.setup_server(command_executor=Mock())
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(200, response.status_code, "HTTP 200 returned")
         with open("testresources/index.html") as index_page:
@@ -26,6 +22,7 @@ class PelicanServerTests(unittest.TestCase):
             self.assertEqual(expected_content, response.data.decode("utf-8"), "Static index.html file returned")
 
     def test_server_returns_index_page(self):
+        self.setup_server(command_executor=Mock())
         response = self.app.get('/index.html', follow_redirects=True)
         self.assertEqual(200, response.status_code, "HTTP 200 returned")
         with open("testresources/index.html") as index_page:
@@ -33,11 +30,48 @@ class PelicanServerTests(unittest.TestCase):
             self.assertEqual(expected_content, response.data.decode("utf-8"), "Static index.html file returned")
 
     def test_server_returns_status(self):
+        self.setup_server(command_executor=Mock())
         response = self.app.get('/status', follow_redirects=True)
         self.assertEqual(200, response.status_code, "HTTP 200 returned")
         response_json = json.loads(response.get_data(as_text=True))
         self.assertTrue("status" in response_json, "Response contains key: status")
         self.assertTrue("lastChange" in response_json, "Response contains key: lastChange")
+
+    def test_server_returns_activation_response_when_activation_occurs(self):
+        status_monitor = Mock()
+        status_monitor.status = Status.DEACTIVATED
+        self.setup_server(status_monitor, Mock())
+        response = self.app.get('/actions/activate', follow_redirects=True)
+        self.assertEqual(200, response.status_code, "HTTP 200 returned")
+        response_json = json.loads(response.get_data(as_text=True))
+        self.assertEqual({"result": "activated"}, response_json, "Response states that the system is now activated")
+
+    def test_server_returns_deactivation_response_when_deactivation_occurs(self):
+        status_monitor = Mock()
+        status_monitor.status = Status.ACTIVATED
+        self.setup_server(status_monitor, Mock())
+        response = self.app.get('/actions/deactivate', follow_redirects=True)
+        self.assertEqual(200, response.status_code, "HTTP 200 returned")
+        response_json = json.loads(response.get_data(as_text=True))
+        self.assertEqual({"result": "deactivated"}, response_json, "Response states that the system is now deactivated")
+
+    def test_server_returns_no_change_response_when_activation_called_and_already_active(self):
+        status_monitor = Mock()
+        status_monitor.status = Status.ACTIVATED
+        self.setup_server(status_monitor, Mock())
+        response = self.app.get('/actions/activate', follow_redirects=True)
+        self.assertEqual(200, response.status_code, "HTTP 200 returned")
+        response_json = json.loads(response.get_data(as_text=True))
+        self.assertEqual({"result": "already activated; no change"}, response_json, "Response states no change")
+
+    def test_server_returns_deactivation_response_when_previously_activated(self):
+        status_monitor = Mock()
+        status_monitor.status = Status.DEACTIVATED
+        self.setup_server(status_monitor, Mock())
+        response = self.app.get('/actions/deactivate', follow_redirects=True)
+        self.assertEqual(200, response.status_code, "HTTP 200 returned")
+        response_json = json.loads(response.get_data(as_text=True))
+        self.assertEqual({"result": "already deactivated; no change"}, response_json, "Response states no change")
 
 
 if __name__ == "__main__":
