097c611fe4feaf38546e9be2707c2783f4e70dac
mikegreen1995 <mikegreen1995@outlook.com>
2020-05-01 13:54:40 +0100

    Add logging to commands.py

--- src/commands.py
+++ src/commands.py
@@ -1,8 +1,11 @@
 import json
+import logging
 import subprocess
 
 
 class CommandExecutor:
+
+    logger = logging.getLogger()
 
     minidlna_restart_command = "sudo service minidlna restart"
     minidlna_stop_command = "sudo service minidlna stop"
@@ -27,18 +30,23 @@ class CommandExecutor:
             self._unmount(device["path"])
 
     def _mount(self, device, mount_point):
+        self.logger.info(f"Mounting '{device}' at '{mount_point}'.")
         subprocess.check_call(self.mount_command_template.format(device=device, mount_point=mount_point), shell=True)
 
     def _unmount(self, device, retries_count=5):
         for i in range(0, retries_count):
             try:
+                self.logger.info(f"Unmounting '{device}'.")
                 subprocess.check_call(self.unmount_command_template.format(device=device), shell=True)
             except subprocess.CalledProcessError:
+                self.logger.info(f"Unmounting '{device}' failed.")
                 continue
             break
 
     def _start_minidlna(self):
+        self.logger.info("Starting minidlna.")
         subprocess.check_call(self.minidlna_restart_command, shell=True)
 
     def _stop_minidlna(self):
+        self.logger.info("Stopping minidlna.")
         subprocess.check_call(self.minidlna_stop_command, shell=True)
--- tests/test_commandexecutor.py
+++ tests/test_commandexecutor.py
@@ -3,6 +3,7 @@ import sys
 import unittest
 
 from commands import CommandExecutor
+from testfixtures import LogCapture
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/')))
 
@@ -16,6 +17,10 @@ class TestCommandExecutor(unittest.TestCase):
         self.command_executor.minidlna_stop_command = null_command
         self.command_executor.mount_command_template = null_command
         self.command_executor.unmount_command_template = null_command
+        self.log_capture = LogCapture()
+
+    def tearDown(self):
+        self.log_capture.uninstall_all()
 
     def test_device_config_loaded_successfully(self):
         expected_device_entry = {
@@ -29,3 +34,17 @@ class TestCommandExecutor(unittest.TestCase):
             }
         self.assertEqual(1, len(self.command_executor.devices))
         self.assertDictEqual(expected_device_entry, self.command_executor.devices[0])
+
+    def test_each_partition_mounted(self):
+        self.command_executor.activate()
+        self.log_capture.check(
+            ("root", "INFO", "Mounting '/dev/sda1' at '/media/my_mount_point'."),
+            ("root", "INFO", "Starting minidlna.")
+        )
+
+    def test_each_device_mounted(self):
+        self.command_executor.deactivate()
+        self.log_capture.check(
+            ("root", "INFO", "Stopping minidlna."),
+            ("root", "INFO", "Unmounting '/dev/sda'.")
+        )
