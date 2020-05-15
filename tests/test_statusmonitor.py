a46549e0d3f64fe47fedd1ebd9c52e3d62900294
mikegreen1995 <mikegreen1995@outlook.com>
2020-05-15 14:35:24 +0100

    Generify status monitor test

--- tests/test_statusmonitor.py
+++ tests/test_statusmonitor.py
@@ -21,26 +21,16 @@ class TestStatusMonitor(unittest.TestCase):
     def test_status_monitor_deactivated_by_default(self):
         self.assertEqual(Status.DEACTIVATED, self.status_monitor.status)
 
-    def test_status_monitor_activated_when_set_active_set_to_true(self):
+    def test_status_monitor_status_changes_successfully(self):
         original_timestamp = self.status_monitor.last_change
-        changed_by_string = "activation_test"
-        self.status_monitor.set_status(Status.ACTIVATED, changed_by=changed_by_string)
-        self.assertEqual(Status.ACTIVATED, self.status_monitor.status)
+        changed_by_string = "test"
+        new_status = Status.ACTIVATED
+        self.status_monitor.set_status(new_status, changed_by=changed_by_string)
+        self.assertEqual(new_status, self.status_monitor.status)
         self.assertGreater(self.status_monitor.last_change, original_timestamp)
         self.assertEqual(self.status_monitor.last_change_by, changed_by_string)
         self.log_capture.check(
-            ("root", "INFO", "New status: ACTIVATED.")
-        )
-
-    def test_status_monitor_activated_when_set_active_set_to_false(self):
-        original_timestamp = self.status_monitor.last_change
-        changed_by_string = "deactivation_test"
-        self.status_monitor.set_status(Status.DEACTIVATED, changed_by=changed_by_string)
-        self.assertEqual(Status.DEACTIVATED, self.status_monitor.status)
-        self.assertGreater(self.status_monitor.last_change, original_timestamp)
-        self.assertEqual(self.status_monitor.last_change_by, changed_by_string)
-        self.log_capture.check(
-            ("root", "INFO", "New status: DEACTIVATED.")
+            ("root", "INFO", "New status: " + new_status.name + ".")
         )
 
 
