04cfb5173c797548b1e37f6e91cec7cfd1f4258d
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2021-03-19 18:35:45 +0000

    Dynamically set absolute paths for test mount paths to work in all environments.

--- tests/test_commandexecutor.py
+++ tests/test_commandexecutor.py
@@ -8,12 +8,17 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../s
 
 from commands import CommandExecutor
 
-DUMMY_MOUNT_POINT = "tests/testresources/dummyMountPoint"
+DUMMY_MOUNT_POINT = os.path.dirname(__file__) + "/testresources/dummyMountPoint"
+
 
 class TestCommandExecutor(unittest.TestCase):
 
     def setUp(self):
-        self.command_executor = CommandExecutor(os.path.dirname(__file__) + "/testresources/devices.json")
+        device_config_file_path = self._populate_device_config_file(
+            os.path.dirname(__file__) + "/testresources/devicesTemplate.json",
+            DUMMY_MOUNT_POINT
+        )
+        self.command_executor = CommandExecutor(device_config_file_path)
         null_command = ":"
         self.command_executor.minidlna_restart_command = null_command
         self.command_executor.minidlna_stop_command = null_command
@@ -32,14 +37,14 @@ class TestCommandExecutor(unittest.TestCase):
 
     def test_device_config_loaded_successfully(self):
         expected_device_entry = {
-                "path": "/dev/sda",
-                "partitions": [
-                    {
-                        "path": "/dev/sda1",
-                        "mountPoint": DUMMY_MOUNT_POINT
-                    }
-                ]
-            }
+            "path": "/dev/sda",
+            "partitions": [
+                {
+                    "path": "/dev/sda1",
+                    "mountPoint": DUMMY_MOUNT_POINT
+                }
+            ]
+        }
         self.assertEqual(1, len(self.command_executor.devices))
         self.assertDictEqual(expected_device_entry, self.command_executor.devices[0])
 
@@ -63,3 +68,12 @@ class TestCommandExecutor(unittest.TestCase):
             ("root", "INFO", "Rescanning libraries."),
             ("root", "INFO", "Restarting minidlna.")
         )
+
+    def _populate_device_config_file(self, template_file_path, mount_point):
+        with open(template_file_path) as template_file:
+            template = template_file.read()
+        contents = template.replace("${MOUNT_POINT}", mount_point)
+        config_file_path = os.path.dirname(template_file_path) + "/devices.json"
+        with open(config_file_path, "w") as config_file:
+            config_file.write(contents)
+        return config_file_path
